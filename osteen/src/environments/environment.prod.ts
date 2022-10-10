bd7df66f073743124ba5d9f710acf20a7b389201
dharmendra <digiprimadharmendra2021@gmail.com>
2022-10-10 18:35:36 +0530

    changes in .gitignore

--- osteen/src/environments/environment.prod.ts
+++ osteen/src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
     production: true,
     hmr: false,
     //apiurl: 'http://139.59.83.155/businessplus/public/api'
-    apiurl: 'http://139.59.83.155/stagebusinessplus/public/api'
+    // apiurl: 'http://139.59.83.155/stagebusinessplus/public/api'
+    apiurl: 'http://139.59.83.155/PicWallPro/public/api'    
 };
